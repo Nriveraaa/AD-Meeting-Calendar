b488f9bc155093bc25d43b906976151f5273a43a
nriveraaa <202310167@fit.edu.ph>
2025-06-24 21:08:09 +0800

    Refactor: Remove inline database connection checker output from index.php

--- index.php
+++ index.php
@@ -1,11 +1,5 @@
 <?php
-echo "<h2>Database Connection Checker</h2>";
-
-// MongoDB Checker
-echo "<strong>MongoDB:</strong><br>";
-require_once __DIR__ . '/mongodbChecker.handler.php';
-
-// PostgreSQL Checker
-echo "<br><strong>PostgreSQL:</strong><br>";
-require_once __DIR__ . '/postgreChecker.handler.php';
+require_once 'bootstrap.php';
+require_once HANDLERS_PATH . 'mongodbChecker.handler.php';
+require_once HANDLERS_PATH . 'postgreChecker.handler.php';
 ?>
