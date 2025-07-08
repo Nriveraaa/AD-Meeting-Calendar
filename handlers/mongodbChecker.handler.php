620f2c7498b9ee3cd61f9f65f7dd07c0e69d2c78
nriveraaa <202310167@fit.edu.ph>
2025-07-08 22:02:17 +0800

    fix: update MongoDB connection to use dynamic host and port from database configuration

--- handlers/mongodbChecker.handler.php
+++ handlers/mongodbChecker.handler.php
@@ -1,11 +1,17 @@
 <?php
+
+require_once UTILS_PATH . '/envSetter.util.php'; // Load .env values
+
 try {
-    $mongo = new MongoDB\Driver\Manager("mongodb://host.docker.internal:27111");
+    $host = $databases['mongoHost'];
+    $port = $databases['mongoPort'];
+
+    $mongo = new MongoDB\Driver\Manager("mongodb://$host:$port");
 
     $command = new MongoDB\Driver\Command(["ping" => 1]);
     $mongo->executeCommand("admin", $command);
 
-    echo "✅ Connected to MongoDB successfully.  <br>";
+    echo "✅ Connected to MongoDB successfully. <br>";
 } catch (MongoDB\Driver\Exception\Exception $e) {
-    echo "❌ MongoDB connection failed: " . $e->getMessage() . "  <br>";
+    echo "❌ MongoDB connection failed: " . $e->getMessage() . " <br>";
 }
