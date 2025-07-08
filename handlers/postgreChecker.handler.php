dcf49f26ec8afce7f5f1811c1ccab8c892e875b8
nriveraaa <202310167@fit.edu.ph>
2025-07-08 22:02:23 +0800

    fix: update PostgreSQL connection to use dynamic values from environment configuration

--- handlers/postgreChecker.handler.php
+++ handlers/postgreChecker.handler.php
@@ -1,19 +1,21 @@
 <?php
 
-$host = "host.docker.internal"; 
-$port = "5112";
-$username = "user";
-$password = "password";
-$dbname = "mymcdatabase";
+require_once UTILS_PATH . '/envSetter.util.php'; // Load .env values
+
+$host = $databases['pgHost'];
+$port = $databases['pgPort'];
+$username = $databases['pgUser'];
+$password = $databases['pgPassword'];
+$dbname = $databases['pgDB'];
 
 $conn_string = "host=$host port=$port dbname=$dbname user=$username password=$password";
 
 $dbconn = pg_connect($conn_string);
 
 if (!$dbconn) {
-    echo "❌ Connection Failed: ", pg_last_error() . "  <br>";
+    echo "❌ Connection Failed: ", pg_last_error() . "<br>";
     exit();
 } else {
-    echo "✔️ PostgreSQL Connection  <br>";
+    echo "✅ PostgreSQL Connection<br>";
     pg_close($dbconn);
 }
