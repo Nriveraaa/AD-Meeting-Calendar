33eee368f69ffd0b7f7b872d2585a21d0228c4b1
nriveraaa <202310167@fit.edu.ph>
2025-07-08 22:29:15 +0800

    fix: update database connection array to use empty strings instead of null

--- utils/envSetter.util.php
+++ utils/envSetter.util.php
@@ -5,13 +5,12 @@ require_once BASE_PATH . '/vendor/autoload.php';
 $dotenv = Dotenv\Dotenv::createImmutable(BASE_PATH);
 $dotenv->load();
 
-// Create $databases array safely
 $databases = [
-    'pgHost' => $_ENV['PG_HOST'] ?? null,
-    'pgPort' => $_ENV['PG_PORT'] ?? null,
-    'pgUser' => $_ENV['PG_USER'] ?? null,
-    'pgPassword' => $_ENV['PG_PASSWORD'] ?? null,
-    'pgDB' => $_ENV['PG_DB'] ?? null,
-    'mongoHost' => $_ENV['MONGO_HOST'] ?? null,
-    'mongoPort' => $_ENV['MONGO_PORT'] ?? null,
+    'pgHost'     => $_ENV['PG_HOST'] ?? '',
+    'pgPort'     => $_ENV['PG_PORT'] ?? '',
+    'pgUser'     => $_ENV['PG_USER'] ?? '',
+    'pgPassword' => $_ENV['PG_PASSWORD'] ?? '',
+    'pgDB'       => $_ENV['PG_DB'] ?? '',
+    'mongoHost'  => $_ENV['MONGO_HOST'] ?? '',
+    'mongoPort'  => $_ENV['MONGO_PORT'] ?? '',
 ];
