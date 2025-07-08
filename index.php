16cc77082c20248a0e1b3a0f8e574b3d9cd661d2
nriveraaa <202310167@fit.edu.ph>
2025-07-08 22:25:45 +0800

    refactor: ensure path constants are only defined if not already set

--- index.php
+++ index.php
@@ -4,10 +4,16 @@ ini_set('display_errors', 1);
 ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
 
-// 2. Define BASE_PATH
-define('BASE_PATH', realpath(__DIR__));
-define('UTILS_PATH', realpath(BASE_PATH . '/utils'));
-define('HANDLERS_PATH', realpath(BASE_PATH . '/handlers'));
+// 2. Define BASE_PATH and other paths (only if not already defined)
+if (!defined('BASE_PATH')) {
+    define('BASE_PATH', realpath(__DIR__));
+}
+if (!defined('UTILS_PATH')) {
+    define('UTILS_PATH', realpath(BASE_PATH . '/utils'));
+}
+if (!defined('HANDLERS_PATH')) {
+    define('HANDLERS_PATH', realpath(BASE_PATH . '/handlers'));
+}
 
 // 3. Include env loader
 require_once UTILS_PATH . '/envSetter.util.php';
